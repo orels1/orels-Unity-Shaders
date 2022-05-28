5af0c4150909f157498c325afe354c6575c959a6
orels <ping@orels.sh>
2022-05-28 22:11:04 +0400

    added shader post processor to auto-export things

---
+++ Editor/ORLShaderPostProcessor.cs
@@ -0,0 +1,28 @@
+﻿using ORL.ModularShaderSystem;
+using UnityEditor;
+using UnityEngine;
+
+namespace ORL
+{
+    public class ORLShaderPostProcessor : AssetPostprocessor
+    {
+        public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+            string[] movedFromAssetPath)
+        {
+            var shaderList = Resources.Load<ORLShaderList>("ORLShaderList");
+            if (shaderList == null) return;
+            foreach (var asset in importedAssets)
+            {
+                if (!asset.EndsWith(".orlshader")) continue;
+                if (!shaderList.shadersList.ContainsKey(asset)) continue;
+                var shaderDefinition = AssetDatabase.LoadAssetAtPath<ORLShaderDefinition>(asset);
+                if (shaderDefinition == null || shaderDefinition.GeneratedShader == null) continue;
+                var filename = asset;
+                filename = filename.Replace('\\', '/');
+                filename = filename.Substring(filename.LastIndexOf("/") + 1);
+                filename = filename.Replace(".orlshader", "");
+                ShaderGenerator.GenerateShader(shaderList.shadersList[asset], shaderDefinition.GeneratedShader, filename);
+            }
+        }
+    }
+}
