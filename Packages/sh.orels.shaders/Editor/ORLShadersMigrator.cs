685ce4974607cb488675612aafce5376d0b8b7b8
orels <ping@orels.sh>
2022-12-26 16:55:50 +0400

    simplified migration logic

--- Packages/sh.orels.shaders/Editor/ORLShadersMigrator.cs
+++ Packages/sh.orels.shaders/Editor/ORLShadersMigrator.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using YamlDotNet.Serialization;
 
 namespace ORL.Shaders
 {
@@ -13,56 +12,12 @@ namespace ORL.Shaders
         [MenuItem("Tools/orels1/Migrate Shaders")]
         public static void ShowWindow()
         {
-            var locator = Resources.Load<TextAsset>("ORLShadersResourceLocator");
-            var locatorPath = AssetDatabase.GetAssetPath(locator);
-            var freshAsset = false;
-            var settingsAsset = Resources.Load<ORLShadersSettings>("ORLShadersSettings");
-            if (settingsAsset == null)
-            {
-                freshAsset = true;
-                settingsAsset = CreateInstance<ORLShadersSettings>();
-                AssetDatabase.CreateAsset(settingsAsset, locatorPath.Substring(0, locatorPath.LastIndexOf("/")) + "/ORLShadersSettings.asset");
-                AssetDatabase.Refresh();
-            }
-            var settingsPath = AssetDatabase.GetAssetPath(settingsAsset);
-            var packagePath = Application.dataPath.Replace("Assets", "") +
-                              settingsPath.Substring(0, settingsPath.LastIndexOf("/"));
-            packagePath = packagePath.Substring(0, packagePath.LastIndexOf("/"));
-            packagePath = packagePath.Substring(0, packagePath.LastIndexOf("/"));
-            packagePath += "/package.json";
-            var jsonString =
-                File.ReadAllLines(packagePath);
-            var version = jsonString.Where(line => line.Contains("\"version\""))
-                .Select(line => line.Trim()).First();
-            version = version.Split(new [] { ": " }, StringSplitOptions.None)[1];
-            version = version.Substring(1);
-            version = version.Substring(0, version.Length - 1);
-            version = version.Replace("\"", "");
-
-            var currVersion = freshAsset ? version : settingsAsset.ShadersVersion;
-
-            if (freshAsset)
-            {
-                settingsAsset.ShadersVersion = version;
-                AssetDatabase.SaveAssets();
-            }
-            
-            ShowWindow(currVersion, version);
-        }
-
-        private static string _oldVersion;
-        private static string _newVersion;
-        public static void ShowWindow(string oldVersion, string newVersion)
-        {
-            _oldVersion = oldVersion;
-            _newVersion = newVersion;
-            
             var window = GetWindow<ORLShadersMigrator>(true);
-            window.titleContent = new GUIContent($"ORL Shaders Migrator ({_oldVersion} -> {_newVersion})");
+            window.titleContent = new GUIContent($"ORL Shaders Migrator");
             window.minSize = new Vector2(300, 500);
             window.Show();
         }
-        
+
         private static HashSet<string> _migrateableShaders = new HashSet<string>
         {
             "02b44cf07259bda4592d27f11536b39d",
@@ -86,7 +41,29 @@ namespace ORL.Shaders
             // "e67e822f851164b40b6b701128914625" // This is Tessellated Displacement, it is currently an old shader, so no migration is needed
         };
 
-        private static string _fileId = "-8512187303908658807";
+        // old GUID -> new GUID
+        private static Dictionary<string, string> _migrationMap = new Dictionary<string, string>()
+        {
+            { "02b44cf07259bda4592d27f11536b39d", "bb1fc5fc14b648debb2f566068d58f43" },
+            { "590a035cb264dbe428f749e03be09bf4", "3944d5378419d984583e5d7cfe7d611d" },
+            { "915eaab166e9b964abd9a9486eef9dac", "81e10a4c59687964aaf903c7612ae9c1" },
+            { "2275fa1837feeec488ff498f874f1f4c", "77a966990a60b6849bcc5ae9e684facc" },
+            { "d76ef0f150281d1498cc80a45d9c5358", "1cd3eff6941570c46a0cca0ae963bd8c" },
+            { "2190dbf828f1685419df234767c5e2c4", "d22b0ac99dbae7e4a9bdca0b613b2464" },
+            { "9edf3bad854dd7f4f8a527bc600d6588", "e224c6e9d43e7c745b42078c6191393f" },
+            { "894debc09c05bd643a49369e54629c0e", "13b2ccd4f6364f84ba5e8fa8fd1698cc" },
+            { "59d2f25a89a34f34fa23f851d7c5d830", "e3221c2c550562449b5c42addb301b40" },
+            { "f3c2bbeecfe6dd246ba0597b41c5d4c4", "b36109bcda611f9498611a60783d4d83" },
+            { "f3ebe765fc449c345849abaeb6ab89f7", "fc636102982bd9548b08929996e8c67e" },
+            { "d6893d669363e58488ed3c131379a0d2", "5664541c8a9b2bc489c9fa4f3d3e81f8" },
+            { "003f099eedad1cc489451a03850c2c12", "029f9b562195c5a488f0744e956fca59" },
+            { "1b03fffd06279344f92579165d1a821c", "f29b375ac6ad8e84f9a5095e5857e957" },
+            { "0caac922b36d9e74e8435b8316baf498", "650f7b5fdb04efe42ac612d9fd0d03cb" },
+            { "5f69cf125658cc046a729a5a5b4b9a11", "7de358116a341004b94136337d239d1b" },
+            { "307b9906231b0784884d4b52416215a8", "10f0534e8e8748a409849338afe43251" },
+        };
+
+        // private static string _fileId = "-8512187303908658807";
 
         private List<string> _affectedMaterials = new List<string>();
         private Vector2 _affectedMatsScrollPos = Vector2.zero;
@@ -99,27 +76,9 @@ namespace ORL.Shaders
                        margin = new RectOffset(10, 10, 10, 10)
                    }))
             {
-                if (_oldVersion == _newVersion)
-                {
-                    EditorGUILayout.LabelField("You have the latest version, migration should not be required!");
-                }
-                else
-                {
-                    EditorGUILayout.LabelField("Shader Migration might be required", EditorStyles.boldLabel);
-                }
                 EditorGUIUtility.fieldWidth = 64;
-                EditorGUILayout.Space(10);
-                using (new EditorGUILayout.HorizontalScope())
-                {
-                    EditorGUILayout.TextField(_oldVersion);
-                    EditorGUILayout.Space(10);
-                    EditorGUILayout.LabelField("->", GUILayout.Width(20));
-                    EditorGUILayout.Space(10);
-                    EditorGUILayout.TextField(_newVersion);
-                }
-                EditorGUILayout.Space(10);
 
-                if (GUILayout.Button("Find Affected Materials", GUILayout.Height(30)))
+                if (GUILayout.Button("Find Materials using ORL Shaders", GUILayout.Height(30)))
                 {
                     _affectedMaterials = FindAffectedMaterials();
                 }
@@ -136,7 +95,7 @@ namespace ORL.Shaders
                             _affectedMatsScrollPos = scroller.scrollPosition;
                             foreach (var mat in _affectedMaterials)
                             {
-                                EditorGUILayout.ObjectField(AssetDatabase.LoadAssetAtPath<Material>(mat), typeof (Material));
+                                EditorGUILayout.ObjectField(AssetDatabase.LoadAssetAtPath<Material>(mat), typeof (Material), false);
                             }
                         }
                     }
@@ -218,7 +177,7 @@ namespace ORL.Shaders
                 var guidLine = shaderLines.First();
                 var shaderGuid = guidLine.Substring(guidLine.IndexOf("guid: ") + 6);
                 shaderGuid = shaderGuid.Substring(0, shaderGuid.IndexOf(","));
-                var shaderPath = AssetDatabase.GUIDToAssetPath(shaderGuid);
+                var shaderPath = AssetDatabase.GUIDToAssetPath(_migrationMap[shaderGuid]);
                 var shader = AssetDatabase.LoadAssetAtPath<Shader>(shaderPath);
                 if (!_dryRun)
                 {
--- Packages/sh.orels.shaders/Editor/ORLShadersSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace ORL.Shaders
-{
-    public class ORLShadersSettings : ScriptableObject
-    {
-        public string ShadersVersion = "4.x";
-    }
-}
