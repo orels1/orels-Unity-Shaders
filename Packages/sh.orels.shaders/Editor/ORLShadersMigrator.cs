3166e3f2117f18107cf3e5a22f5febf6318a679e
orels <ping@orels.sh>
2023-01-03 15:13:26 +0400

    added a name map to the migrator for better migration experience

--- Packages/sh.orels.shaders/Editor/ORLShadersMigrator.cs
+++ Packages/sh.orels.shaders/Editor/ORLShadersMigrator.cs
@@ -66,7 +66,7 @@ namespace ORL.Shaders
         };
 
         // old GUID -> new GUID
-        private static Dictionary<string, string> _migrationMap = new Dictionary<string, string>()
+        private static Dictionary<string, string> _migrationMap = new Dictionary<string, string>
         {
             { "02b44cf07259bda4592d27f11536b39d", "bb1fc5fc14b648debb2f566068d58f43" },
             { "590a035cb264dbe428f749e03be09bf4", "3944d5378419d984583e5d7cfe7d611d" },
@@ -88,7 +88,7 @@ namespace ORL.Shaders
         };
         
         // new GUID -> old GUID
-        private static Dictionary<string, string> _revertMap = new Dictionary<string, string>()
+        private static Dictionary<string, string> _revertMap = new Dictionary<string, string>
         {
             { "bb1fc5fc14b648debb2f566068d58f43", "02b44cf07259bda4592d27f11536b39d" },
             { "3944d5378419d984583e5d7cfe7d611d", "590a035cb264dbe428f749e03be09bf4" },
@@ -107,6 +107,28 @@ namespace ORL.Shaders
             { "650f7b5fdb04efe42ac612d9fd0d03cb", "0caac922b36d9e74e8435b8316baf498" },
             { "7de358116a341004b94136337d239d1b", "5f69cf125658cc046a729a5a5b4b9a11" },
             { "10f0534e8e8748a409849338afe43251", "307b9906231b0784884d4b52416215a8" },
+        };
+        
+        // name -> old GUID
+        private static Dictionary<string, string> _nameMap = new Dictionary<string, string>
+        {
+            { "orels1/Standard", "02b44cf07259bda4592d27f11536b39d" },
+            { "orels1/Standard AudioLink", "590a035cb264dbe428f749e03be09bf4" },
+            { "orels1/Standard Color Randomisation", "915eaab166e9b964abd9a9486eef9dac" },
+            { "orels1/Standard Cutout", "2275fa1837feeec488ff498f874f1f4c" },
+            { "orels1/Standard Glass", "d76ef0f150281d1498cc80a45d9c5358" },
+            { "orels1/Standard Layered Material Triplanar Effects", "2190dbf828f1685419df234767c5e2c4" },
+            { "orels1/Standard Layered Material", "9edf3bad854dd7f4f8a527bc600d6588" },
+            { "orels1/Standard Layered Parallax", "894debc09c05bd643a49369e54629c0e" },
+            { "orels1/Standard LTCGI", "59d2f25a89a34f34fa23f851d7c5d830" },
+            { "orels1/Standard Neon Light", "f3c2bbeecfe6dd246ba0597b41c5d4c4" },
+            { "orels1/Standard Triplanar Effects", "f3ebe765fc449c345849abaeb6ab89f7" },
+            { "orels1/Standard Vertex Animation", "d6893d669363e58488ed3c131379a0d2" },
+            { "orels1/Standard Vertical Fog", "003f099eedad1cc489451a03850c2c12" },
+            { "orels1/Toon/Main", "1b03fffd06279344f92579165d1a821c" },
+            { "orels1/VFX/Block Fader", "0caac922b36d9e74e8435b8316baf498" },
+            { "orels1/VFX/Clouds", "5f69cf125658cc046a729a5a5b4b9a11" },
+            { "orels1/VFX/Cubemap Screen", "307b9906231b0784884d4b52416215a8" }
         };
 
         // private static string _fileId = "-8512187303908658807";
@@ -224,16 +246,33 @@ namespace ORL.Shaders
                     i++;
                     continue;
                 }
+                
+                if (_nameMap.ContainsKey(material.shader.name))
+                {
+                    affectedMaterials.Add(path);
+                    i++;
+                    continue;
+                }
                 var matYamlSource = File.ReadAllLines(Application.dataPath.Replace("\\", "/").Replace("Assets", "") + path);
-                var shaderLines = matYamlSource.Where(line => line.Trim().StartsWith("m_Shader")).ToList();
+                var shaderLines = matYamlSource.Where(line => line.Trim().StartsWith("m_Shader:")).ToList();
                 if (shaderLines.Count == 0)
                 {
                     i++;
                     continue;
                 }
                 var guidLine = shaderLines.First();
-                var shaderGuid = guidLine.Substring(guidLine.IndexOf("guid: ") + 6);
-                shaderGuid = shaderGuid.Substring(0, shaderGuid.IndexOf(","));
+                if (!guidLine.Contains("guid: "))
+                {
+                    i++;
+                    continue;
+                }
+                var shaderGuid = guidLine.Substring(guidLine.IndexOf("guid: ", StringComparison.InvariantCulture) + 6);
+                if (!shaderGuid.Contains(","))
+                {
+                    i++;
+                    continue;
+                }
+                shaderGuid = shaderGuid.Substring(0, shaderGuid.IndexOf(",", StringComparison.InvariantCulture));
 
                 if (!(revert ? _revertableShaders : _migrateableShaders).Contains(shaderGuid))
                 {
@@ -287,9 +326,40 @@ namespace ORL.Shaders
 
                     EditorUtility.DisplayProgressBar("Migrating Materials", $"Migrating {path}...",
                         i / _affectedMaterials.Count);
-                    var guidLine = shaderLines.First();
-                    var shaderGuid = guidLine.Substring(guidLine.IndexOf("guid: ") + 6);
-                    shaderGuid = shaderGuid.Substring(0, shaderGuid.IndexOf(","));
+                    var shaderGuid = "";
+                    if (_nameMap.ContainsKey(mat.shader.name))
+                    {
+                        if (revert)
+                        {
+                            shaderGuid = _migrationMap[_nameMap[mat.shader.name]];
+                        }
+                        else
+                        {
+                            shaderGuid = _nameMap[mat.shader.name];
+                        }
+                    }
+                    else
+                    {
+                        var guidLine = shaderLines.First();
+                        if (!guidLine.Contains("guid: "))
+                        {
+                            EditorUtility.DisplayProgressBar("Migrating Materials", $"Skipping {path}..",
+                                i / _affectedMaterials.Count);
+                            i++;
+                            invalidCount++;
+                            continue;
+                        }
+                        shaderGuid = guidLine.Substring(guidLine.IndexOf("guid: ", StringComparison.InvariantCulture) + 6);
+                        if (!shaderGuid.Contains(","))
+                        {
+                            EditorUtility.DisplayProgressBar("Migrating Materials", $"Skipping {path}..",
+                                i / _affectedMaterials.Count);
+                            i++;
+                            invalidCount++;
+                            continue;
+                        }
+                        shaderGuid = shaderGuid.Substring(0, shaderGuid.IndexOf(",", StringComparison.InvariantCulture));
+                    }
                     var newGuid = revert ? _revertMap[shaderGuid] : _migrationMap[shaderGuid];
                     var shaderPath = AssetDatabase.GUIDToAssetPath(newGuid);
                     var shader = AssetDatabase.LoadAssetAtPath<Shader>(shaderPath);
@@ -323,7 +393,7 @@ namespace ORL.Shaders
             {
                 Undo.CollapseUndoOperations(group);
                 EditorUtility.ClearProgressBar();
-                if (!_dryRun)
+                if (_dryRun)
                 {
                     Debug.Log($"DRY RUN. Migration finished. Would have migrated {migratedCount} / {_affectedMaterials.Count} materials. Could not find shaders for {missedCount} / {_affectedMaterials.Count} materials. Invalid materials {invalidCount} / {_affectedMaterials.Count}.");
                 }
