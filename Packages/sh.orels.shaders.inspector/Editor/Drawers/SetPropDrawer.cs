6f70e67a62711633ad6a3754a2218af9f809c2fd
orels <ping@orels.sh>
2023-07-27 06:56:32 +0400

    added SetProp drawer support

--- Packages/sh.orels.shaders.inspector/Editor/Drawers/SetPropDrawer.cs
+++ Packages/sh.orels.shaders.inspector/Editor/Drawers/SetPropDrawer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using ORL.ShaderConditions;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,8 +11,11 @@ namespace ORL.Drawers
     {
         public string FunctionName => "SetProp";
         
-        // Matches %SetKeyword(_TextureVar, KEYWORD_NAME)
-        private Regex _matcher = new Regex(@"%SetKeyword\((?<texture>[\w]+)+,\s*(?<keyword>[\w]+)\)");
+        // Matches %SetProp((Shader Condition), PropName, Value when True, Value when False)
+        private Regex _matcher = new Regex(@"%SetProp\((?<condition>\([\w\,\s\&\|\(\)\!\<\>\=]+\))+\s*,\s*(?<propName>[\w]+)+\s*,\s*(?<trueValue>[\d\.]+)\s*,\s*(?<falseValue>[\d\.]+)\s*\)");
+        
+        private Dictionary<string, Expression> _expressions = new Dictionary<string, Expression>();
+        private Interpreter _interpreter;
         
         public string[] PersistentKeys => Array.Empty<string>();
 
@@ -20,14 +24,38 @@ namespace ORL.Drawers
             if (EditorGUI.indentLevel == -1) return true;
             
             var match = _matcher.Match(property.displayName);
-            var keyword = match.Groups["keyword"].Value;
-            var texture = match.Groups["texture"].Value;
-            foreach (Material material in editor.targets)
+            
+            if (_interpreter == null)
             {
-                if (material.GetTexture(texture) != null)
-                    material.EnableKeyword(keyword);
-                else
-                    material.DisableKeyword(keyword);
+                _interpreter = new Interpreter(editor.target as Material);
+            }
+            Compiler.HasError = false;
+            var condition = match.Groups["condition"].Value;
+            condition = condition.Substring(1, condition.Length - 2);
+            var propName = match.Groups["propName"].Value;
+            var trueValue = match.Groups["trueValue"].Value;
+            var falseValue = match.Groups["falseValue"].Value;
+
+            if (!_expressions.ContainsKey(property.displayName))
+            {
+                var scanner = new Scanner(condition);
+                var tokens = scanner.Tokenize();
+                var parser = new Parser(tokens);
+                var expr = parser.Parse();
+                if (Compiler.HasError)
+                {
+                    Debug.LogError("Failed to parse condition: " + condition);
+                    return true;
+                }
+                _expressions.Add(property.displayName, expr);
+            }
+            var result = Convert.ToBoolean(_interpreter.Interpret(_expressions[property.displayName]));
+
+            var value = result ? trueValue : falseValue;
+
+            if (float.TryParse(value, out var parsedValue) && Mathf.Abs((editor.target as Material).GetFloat(propName) - parsedValue) > float.Epsilon)
+            {
+                (editor.target as Material)?.SetFloat(propName, float.Parse(value));
             }
 
             return next();
