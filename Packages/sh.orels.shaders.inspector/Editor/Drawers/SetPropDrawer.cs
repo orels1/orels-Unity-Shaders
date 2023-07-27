3bd2c405774c60b078c69aeefc78b1526d924a42
orels <ping@orels.sh>
2023-07-27 05:52:43 +0400

    improved outlines and added facing support

---
+++ Packages/sh.orels.shaders.inspector/Editor/Drawers/SetPropDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace ORL.Drawers
+{
+    public class SetPropDrawer: IDrawerFunc
+    {
+        public string FunctionName => "SetProp";
+        
+        // Matches %SetKeyword(_TextureVar, KEYWORD_NAME)
+        private Regex _matcher = new Regex(@"%SetKeyword\((?<texture>[\w]+)+,\s*(?<keyword>[\w]+)\)");
+        
+        public string[] PersistentKeys => Array.Empty<string>();
+
+        public bool OnGUI(MaterialEditor editor, MaterialProperty[] properties, MaterialProperty property, int index, ref Dictionary<string, object> uiState, Func<bool> next)
+        {
+            if (EditorGUI.indentLevel == -1) return true;
+            
+            var match = _matcher.Match(property.displayName);
+            var keyword = match.Groups["keyword"].Value;
+            var texture = match.Groups["texture"].Value;
+            foreach (Material material in editor.targets)
+            {
+                if (material.GetTexture(texture) != null)
+                    material.EnableKeyword(keyword);
+                else
+                    material.DisableKeyword(keyword);
+            }
+
+            return next();
+        }
+    }
+}
