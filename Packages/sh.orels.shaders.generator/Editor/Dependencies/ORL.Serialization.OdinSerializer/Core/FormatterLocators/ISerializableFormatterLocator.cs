5f664bb7e0662dd77fe118f8dbd40f6566aa4c4f
orels1 <ping@orels.sh>
2024-03-03 23:02:51 +0400

    added odin and some basic shader anlysis tools

---
+++ Packages/sh.orels.shaders.generator/Editor/Dependencies/ORL.Serialization.OdinSerializer/Core/FormatterLocators/ISerializableFormatterLocator.cs
@@ -0,0 +1,57 @@
+//-----------------------------------------------------------------------
+// <copyright file="ISerializableFormatterLocator.cs" company="Sirenix IVS">
+// Copyright (c) 2018 Sirenix IVS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using ORL.Serialization.OdinSerializer;
+
+[assembly: RegisterFormatterLocator(typeof(ISerializableFormatterLocator), -110)]
+
+namespace ORL.Serialization.OdinSerializer
+{
+    using Utilities;
+    using System;
+    using System.Runtime.Serialization;
+
+    internal class ISerializableFormatterLocator : IFormatterLocator
+    {
+        public bool TryGetFormatter(Type type, FormatterLocationStep step, ISerializationPolicy policy, bool allowWeakFallbackFormatters, out IFormatter formatter)
+        {
+            if (step != FormatterLocationStep.AfterRegisteredFormatters || !typeof(ISerializable).IsAssignableFrom(type))
+            {
+                formatter = null;
+                return false;
+            }
+
+            try
+            {
+                formatter = (IFormatter)Activator.CreateInstance(typeof(SerializableFormatter<>).MakeGenericType(type));
+            }
+            catch (Exception ex)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                if (allowWeakFallbackFormatters && (ex is ExecutionEngineException || ex.GetBaseException() is ExecutionEngineException))
+#pragma warning restore CS0618 // Type or member is obsolete
+                {
+                    formatter = new WeakSerializableFormatter(type);
+                }
+                else throw;
+            }
+
+            return true;
+        }
+    }
+}
