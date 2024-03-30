4cd26b6ebdb12dfb4c8644fcbd613c204716469f
orels1 <ping@orels.sh>
2024-03-31 00:17:18 +0400

    Dev project moved to unity 2022
    - Also removed VPM resolver (not needed anymore)
    - Temporarily disabled release exported until i move it away from VPM resolver (need to use regular System Text Json with a minimal struct
    more 2022 changes
    more layout stuff
    
    I should probably just remove this :sweat:
    removed per-user things

--- Assets/Tools/Editor/ReleaseExporter.cs
+++ Assets/Tools/Editor/ReleaseExporter.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using UnityEditor;
 using UnityEngine;
-using VRC.PackageManagement.Core.Types.Packages;
+// using VRC.PackageManagement.Core.Types.Packages;
 
 namespace ORL.Tools
 {
@@ -15,45 +15,45 @@ namespace ORL.Tools
         };
 
         [MenuItem("Tools/orels1/Export Release")]
-        private static void ExportAsUnityPackage ()
+        private static void ExportAsUnityPackage()
         {
-            var manifestPath = Path.Combine(_exportFolders[0], VRCPackageManifest.Filename);
-            var manifest = VRCPackageManifest.GetManifestAtPath(manifestPath);
-            if (manifest == null)
-            {
-                Debug.LogError("Failed to load main package manifest to extract version, aborting");
-                return;
-            }
+            // var manifestPath = Path.Combine(_exportFolders[0], VRCPackageManifest.Filename);
+            // var manifest = VRCPackageManifest.GetManifestAtPath(manifestPath);
+            // if (manifest == null)
+            // {
+            //     Debug.LogError("Failed to load main package manifest to extract version, aborting");
+            //     return;
+            // }
 
-            var exportDir = Path.Combine(Directory.GetCurrentDirectory(), "Exports");
-            Directory.CreateDirectory(exportDir);
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders, 
-                Path.Combine(exportDir, $"orl-shaders-combined-{manifest.Version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
-            
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders[0], 
-                Path.Combine(exportDir, $"sh.orels.shaders-standalone-{manifest.Version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
-            
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders[1], 
-                Path.Combine(exportDir, $"sh.orels.shaders.inspector-standalone-{manifest.Version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
-            
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders[2], 
-                Path.Combine(exportDir, $"sh.orels.shaders.generator-standalone-{manifest.Version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
+            // var exportDir = Path.Combine(Directory.GetCurrentDirectory(), "Exports");
+            // Directory.CreateDirectory(exportDir);
+            // AssetDatabase.ExportPackage
+            // (
+            //     _exportFolders, 
+            //     Path.Combine(exportDir, $"orl-shaders-combined-{manifest.Version}.unitypackage"),
+            //     ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
+            // );
+
+            // AssetDatabase.ExportPackage
+            // (
+            //     _exportFolders[0], 
+            //     Path.Combine(exportDir, $"sh.orels.shaders-standalone-{manifest.Version}.unitypackage"),
+            //     ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
+            // );
+
+            // AssetDatabase.ExportPackage
+            // (
+            //     _exportFolders[1], 
+            //     Path.Combine(exportDir, $"sh.orels.shaders.inspector-standalone-{manifest.Version}.unitypackage"),
+            //     ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
+            // );
+
+            // AssetDatabase.ExportPackage
+            // (
+            //     _exportFolders[2], 
+            //     Path.Combine(exportDir, $"sh.orels.shaders.generator-standalone-{manifest.Version}.unitypackage"),
+            //     ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
+            // );
         }
     }
 }
