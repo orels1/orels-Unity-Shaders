ec6cf49b7e142e67e0020ed6aaf694ae687cef27
orels1 <ping@orels.sh>
2024-07-05 01:29:19 +0400

    reworked the release exporter to ignore files new to git

--- Assets/Tools/Editor/ReleaseExporter.cs
+++ Assets/Tools/Editor/ReleaseExporter.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace ORL.Tools
 {
@@ -33,37 +37,64 @@ namespace ORL.Tools
                 return;
             }
 
+            // Get a list of new files from git
+            var processInfo = new ProcessStartInfo("git", "status -s")
+            {
+                WorkingDirectory = new FileInfo(".").FullName,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true
+            };
+            var gitProc = Process.Start(processInfo);
+            gitProc.WaitForExit();
+            if (gitProc.ExitCode != 0)
+            {
+                Debug.LogError("Failed to get git status, aborting");
+                return;
+            }
+            var ignored = new List<string>();
+            while (!gitProc.StandardOutput.EndOfStream)
+            {
+                var line = gitProc.StandardOutput.ReadLine();
+                if (line.StartsWith("??"))
+                {
+                    ignored.Add(line.Substring(2).Trim().Replace('/', '\\'));
+                }
+            }
+
             Debug.Log($"Exporting version {manifest.version}");
 
             var exportDir = Path.Combine(Directory.GetCurrentDirectory(), "Exports");
             Directory.CreateDirectory(exportDir);
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders,
-                Path.Combine(exportDir, $"orl-shaders-combined-{manifest.version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
 
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders[0],
-                Path.Combine(exportDir, $"sh.orels.shaders-standalone-{manifest.version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
+            ExportAsUnityPackage(_exportFolders, ignored, Path.Combine(exportDir, $"sh.orels.shaders-combined-{manifest.version}.unitypackage"), manifest.version);
+            ExportAsUnityPackage(_exportFolders[0], ignored, Path.Combine(exportDir, $"sh.orels.shaders-standalone-{manifest.version}.unitypackage"), manifest.version);
+            ExportAsUnityPackage(_exportFolders[1], ignored, Path.Combine(exportDir, $"sh.orels.shaders.inspector-standalone-{manifest.version}.unitypackage"), manifest.version);
+            ExportAsUnityPackage(_exportFolders[2], ignored, Path.Combine(exportDir, $"sh.orels.shaders.generator-standalone-{manifest.version}.unitypackage"), manifest.version);
 
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders[1],
-                Path.Combine(exportDir, $"sh.orels.shaders.inspector-standalone-{manifest.version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
+            // Open the export folder
+            var exportedPath = new FileInfo("Exports").FullName;
+            Process.Start(exportedPath);
+        }
 
-            AssetDatabase.ExportPackage
-            (
-                _exportFolders[2],
-                Path.Combine(exportDir, $"sh.orels.shaders.generator-standalone-{manifest.version}.unitypackage"),
-                ExportPackageOptions.Recurse | ExportPackageOptions.Interactive
-            );
+        private static void ExportAsUnityPackage(string[] baseFolders, List<string> ingored, string exportPath, string version)
+        {
+            var list = baseFolders.SelectMany(f => Directory.GetFiles(f, "*", SearchOption.AllDirectories))
+                .Select(f => f.Replace('/', '\\'))
+                .Where(f => !ingored.Any(i => f.Contains(i, StringComparison.InvariantCultureIgnoreCase)))
+                .ToArray();
+
+            Debug.Log("Packages\\sh.orels.shaders\\Editor\\Dependencies\\ORLLayoutToolkit.cs".Contains("Packages\\sh.orels.shaders\\Editor\\Dependencies\\"));
+            AssetDatabase.ExportPackage(list, exportPath, ExportPackageOptions.Recurse);
+        }
+
+        private static void ExportAsUnityPackage(string baseFolder, List<string> ingored, string exportPath, string version)
+        {
+            var list = Directory.GetFiles(baseFolder, "*", SearchOption.AllDirectories)
+                .Select(f => f.Replace('/', '\\'))
+                .Where(f => !ingored.Any(i => f.Contains(i, StringComparison.InvariantCultureIgnoreCase)))
+                .ToArray();
+            AssetDatabase.ExportPackage(list, exportPath, ExportPackageOptions.Recurse);
         }
     }
 }
