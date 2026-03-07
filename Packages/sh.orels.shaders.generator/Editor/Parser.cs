e14dd3c36ca39d212d84f0649dd52ead799cd271
orels1 <ping@orels.sh>
2026-03-07 15:00:00 -0800

    fixed an issue with parenthesis in Shader Name
    - fixes #111

--- Packages/sh.orels.shaders.generator/Editor/Parser.cs
+++ Packages/sh.orels.shaders.generator/Editor/Parser.cs
@@ -249,7 +249,7 @@ namespace ORL.ShaderGenerator
 
                                 var blockIndentation = _current;
                                 _start = _current + 1;
-                                var paramsString = ConsumeUntil(')');
+                                var paramsString = ConsumeParameters();
                                 var paramsList = new List<string>();
                                 if (!string.IsNullOrEmpty(paramsString))
                                 {
@@ -451,6 +451,37 @@ namespace ORL.ShaderGenerator
                 }
 
                 result.Add(cleanLine.Substring(string.IsNullOrWhiteSpace(cleanLine) ? 0 : offset));
+            }
+
+            return null;
+        }
+
+        private string ConsumeParameters()
+        {
+            var initial = _current;
+            var depth = 0;
+            while (_current < _total)
+            {
+                if (_current + 1 > _total)
+                {
+                    return null;
+                }
+
+                if (_currentLine[_current] == '(')
+                {
+                    depth++;
+                }
+                else if (_currentLine[_current] == ')')
+                {
+                    depth--;
+                }
+
+                if (_current != initial && depth == 0)
+                {
+                    return _currentLine.Substring(_start, _current - _start);
+                }
+                
+                _current++;
             }
 
             return null;
